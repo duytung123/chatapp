c16fb086805af7488560b7d9d535c17c5794d90a
= <=>
2021-04-04 21:03:36 +0700

    hello

---
+++ users.php
@@ -0,0 +1,81 @@
+<?php 
+  session_start();
+  include_once "php/config.php";
+  if(!isset($_SESSION['unique_id'])){
+
+
+    header("location: login.php");
+
+     $outgoing_id = $_SESSION['unique_id'];
+        $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
+        $output = "";
+        $sql = "SELECT * FROM messages LEFT JOIN users ON users.unique_id = messages.outgoing_msg_id
+                WHERE (outgoing_msg_id = {$outgoing_id} AND incoming_msg_id = {$incoming_id})
+                OR (outgoing_msg_id = {$incoming_id} AND incoming_msg_id = {$outgoing_id}) ORDER BY msg_id";
+        $query = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($query) > 0){
+            while($row = mysqli_fetch_assoc($query)){
+                if($row['outgoing_msg_id'] === $outgoing_id){
+                    $output .= '<div class="chat outgoing">
+                                <div class="details">
+                                    <p>'. $row['msg'] .'</p>
+                                </div>
+                                </div>';
+                }else{
+                    $output .= '<div class="chat incoming">
+                                <img src="php/images/'.$row['img'].'" alt="">
+                                <div class="details">
+                                    <p>'. $row['msg'] .'</p>
+                                </div>
+                                </div>';
+                }
+            }
+        }else{
+            $output .= '<div class="text">No messages are available. Once you send message they will appear here.</div>';
+        }
+        echo $output;
+  }
+?>
+
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="users">
+      <header>
+        <div class="content">
+          <?php 
+            $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+            if(mysqli_num_rows($sql) > 0){
+              $row = mysqli_fetch_assoc($sql);
+            }
+          ?>
+          <img src="php/images/<?php echo $row['img']; ?>" alt="">
+          <div class="details">
+            <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+            <p><?php echo $row['status']; ?></p>
+
+          </div>
+        </div>
+        <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a>
+      </header>
+      <div class="search">
+        <span class="text">Select an user to start chat</span>
+        <input type="text" placeholder="Enter name to search...">
+        <button><i class="fas fa-search"></i></button>
+      </div>
+      <div class="users-list">
+  
+      </div>
+    </section>
+  </div>
+
+
+    <div class="notification">
+                        <span>nguyễn duy tùng</span>
+                        <p><?php echo $row['msg']; ?></p>
+                    </div>
+                
+  <script src="javascript/users.js"></script>
+
+</body>
+</html>
